38ec90d71ad11465e55f5270f304cf0330fe0953
Kalpana-Patwal <kalpanapatwal1220@gmail.com>
2024-05-04 16:06:30 +0530

    FirstCommit

---
+++ basic.js
@@ -0,0 +1,28 @@
+// var b="kalpana";
+// let c="hi";
+// c="ok";
+// {
+// let b=12;
+// console.log(b);
+// }
+ 
+// console.log(b);
+// console.log(c);
+// let w="ehy";
+// let y=5;
+// console.log(w,y,b,c);
+// console.log(typeof b);
+// let g;
+// console.log(g);
+
+
+// let a={harry:98,rohan:70,aakash:7};
+// for(let i=0;i<Object.keys.apply(a).length;i++){
+//     console.log(Object.keys(marks[i])+a[Object.keys(marks)[i]]);
+// }
+let cn=4;
+let i;
+while(i!=n){
+i=prompt("enter a number"); 
+}
+console.log("correct");
